59485fdcb051c58d38bcce70d24d415aefd228b1
webspaci <108552186+webspaci@users.noreply.github.com>
2023-12-22 19:00:47 +0330

    sdsd
    sdsds

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1703258982,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/pinkform-sheet0.png",
+		"images/pinklayer-sheet0.png",
+		"images/pinklayer-sheet1.png",
+		"images/hearts-sheet0.png",
+		"images/hearts-sheet1.png",
+		"images/returnback-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
